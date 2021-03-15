f35da138557ade9ce1c65402832fe2cdfc801985
Piotr Zalewa <zaloon@gmail.com>
2021-03-15 17:26:58 +0100

    recognizing mood

--- pages/components/Camera.js
+++ pages/components/Camera.js
@@ -1,28 +1,70 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
+const THRESHOLD = 0.7
+
+const startModels = async () => {
+  return Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  ])
+}
 
 const startVideo = (video) => {
-	navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" }, audio: false })
-	.then( stream => video.srcObject = stream )
-	.catch(err => console.error(err))
-}
-const Camera = (props) => {
-	const videoRef = useRef()
-	const [ started, setStarted ] = useState(false)
-	const width = props.width || 720
-	const height = props.height || 560
-	useEffect(() => {
-		if (!started) {
-			startVideo(videoRef.current)
-		}
-	})
-	return <video ref={videoRef} width={width} height={height} autoPlay muted></video>
+  console.log('--- starting video')
+  navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' }, audio: false })
+    .then(function (stream) {
+      video.current.srcObject = stream
+    })
+    .catch(err => console.error(err))
 }
 
+const Camera = React.forwardRef((props, ref) => {
+  const [isStarted, setStarted] = useState(false)
+  const [expressions, setExpressions] = useState([])
+  const width = props.width || 720
+  const height = props.height || 560
+
+  const detectMood = (evt) => {
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(
+        evt.target,
+        new faceapi.TinyFaceDetectorOptions()
+      )
+        .withFaceLandmarks()
+        .withFaceExpressions()
+      if (detections && detections.length) {
+        const mood = Object.entries(detections[0].expressions).filter(
+          expression => expression[1] > THRESHOLD)
+        setExpressions(mood.map(e => e[0]))
+      }
+    }, 100)
+  }
+  useEffect(async () => {
+    if (!isStarted) {
+      setTimeout(async () => {
+        console.log('--- loading face-api models')
+        await startModels()
+        console.log('--- models loaded')
+        startVideo(ref)
+        setStarted(true)
+      }, 300)
+    }
+  })
+
+  return (<>
+    <p>current mood --&gt; {expressions && expressions.map(expression => <>{expression}</>)}</p>
+    <video ref={ref} width={width} height={height} onPlay={detectMood} autoPlay muted></video>
+  </>)
+})
+
+Camera.displayName = 'Camera'
+
 Camera.propTypes = {
-	width: PropTypes.number,
-	height: PropTypes.number
+  width: PropTypes.number,
+  height: PropTypes.number
 }
 
 export default Camera
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
+import React, { useRef } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Camera from './components/Camera'
 
-export default function Home() {
+export default function Home () {
+  const videoRef = useRef()
   return (
     <div className={styles.container}>
       <Head>
         <title>Mood Lifter</title>
         <link rel="icon" href="/favicon.ico" />
+        <script defer src="/face-api.min.js"></script>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
           Mood Lifter
         </h1>
-        <Camera />
+        <Camera ref={videoRef} />
         <p className={styles.description}>
           We will lift your mood in a second! (or two)
         </p>
-
-       
       </main>
 
       <footer className={styles.footer}>
