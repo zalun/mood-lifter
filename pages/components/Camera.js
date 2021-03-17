53b6b95d6579cbbeec23adccd19ea4888f521aae
Piotr Zalewa <zaloon@gmail.com>
2021-03-17 14:50:53 +0100

    Clearing detection after we're confident about an expression.
    New one starts in 5s

--- pages/components/Camera.js
+++ pages/components/Camera.js
@@ -8,6 +8,9 @@ import PropTypes from 'prop-types'
 // How confident about expressions should be the network to consider it
 const THRESHOLD = 0.5
 
+let isExpressionDetected = null
+let detectionId = null
+
 const startModels = async () => {
   return Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -22,15 +25,16 @@ const startVideo = (video) => {
   // TODO: specify the camera resolution
   navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' }, audio: false })
     .then(function (stream) {
-      video.current.srcObject = stream
+      video.srcObject = stream
     })
     .catch(err => console.error(err))
 }
 
-const Camera = React.forwardRef((props, ref) => {
+const Camera = React.forwardRef((props, videoRef) => {
   // Prevent from starting the face recognition multiple times
   const [isStarted, setStarted] = useState(false)
   // Hold the recognized expressions in a state
+  const [expression, setExpression] = useState('')
   const [expressions, setExpressions] = useState([])
   // Status is placed in global store
   const [status, setStatus] = useStore('status')
@@ -39,44 +43,60 @@ const Camera = React.forwardRef((props, ref) => {
   const width = props.width || 240
   const height = props.height || 160
 
-  const detectMood = (evt) => {
-    setInterval(async () => {
-      // Check the expression every 100 ms
-      const detections = await faceapi.detectAllFaces(
-        evt.target,
-        new faceapi.TinyFaceDetectorOptions()
-      )
-        .withFaceLandmarks()
-        .withFaceExpressions()
-      if (detections && detections.length) {
-        // set expressions state
-        setExpressions(detections[0].expressions)
-      }
-    }, 100)
+  const readExpression = async () => {
+    const detections = await faceapi.detectAllFaces(
+      videoRef.current,
+      new faceapi.TinyFaceDetectorOptions()
+    )
+      .withFaceLandmarks()
+      .withFaceExpressions()
+    if (detections && detections.length) {
+      // set expressions state
+      const all = Object.entries(detections[0].expressions).map(exp => { return { name: exp[0], score: exp[1] } })
+      const chosen = all.filter(exp => exp.score > THRESHOLD).map(exp => exp.name).pop()
+      isExpressionDetected = chosen
+      setExpression(chosen)
+      setExpressions(all)
+    }
+  } 
+  const detectMood = () => {
+    setStatus('detecting face expression...')
+    // Check the expression every some ms
+    isExpressionDetected = null
+    detectionId = setInterval(readExpression, 200)
+    console.log('created new interval', detectionId)
   }
+  
   useEffect(async () => {
-    // Call this function after the component is mounted.
+    console.log('state changed or something', isExpressionDetected)
+    // Load components only once.
     if (!isStarted) {
       setTimeout(async () => {
+        setStarted(true)
+        videoRef.current.addEventListener('play', detectMood)
         setStatus('loading face-api models...')
         await startModels()
         setStatus('starting video...')
-        startVideo(ref)
-        setStarted(true)
-        setStatus('')
+        startVideo(videoRef.current)
       }, 300)
+    }
+    if (isExpressionDetected) {
+      isExpressionDetected = false
+      clearInterval(detectionId)
+      setStatus('Detecting expression stopped. Next in 5 seconds')
+      console.log('cleared interval', detectionId)
+      setTimeout(detectMood, 5000)
     }
   })
 
-  const all = Object.entries(expressions).map(expression => { return { name: expression[0], score: expression[1] } })
-  const chosen = all.filter(expression => expression.score > THRESHOLD).map(expression => expression.name)
+
 
   // Render the video (the paragraph is just for the presentation while development)
   return (<>
-    <p>{chosen}</p>
-    <video ref={ref} width={width} height={height} onPlay={detectMood} autoPlay muted></video>
-    <p>{all && all.map(expression => {
-      return <>{expression.name}: {Math.round(expression.score * 100)}% </>
+    <p>{expression}</p>
+    <video ref={videoRef} width={width} height={height} autoPlay muted></video>
+    <p>{expressions && expressions.map(exp => {
+      return <span key={exp.name}>{exp.name}: {Math.round(exp.score * 100)}% </span>
     })}</p>
 
   </>)
