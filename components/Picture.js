d5e0887f7ef5c83823894eba8a39b101fc163c91
Piotr Zalewa <zaloon@gmail.com>
2021-03-22 13:57:33 +0100

    using Material UI to render da buney

--- components/Picture.js
+++ components/Picture.js
@@ -1,18 +1,73 @@
 // Display a picture based on expressions
-import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react'
+import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
+import { Fab, Modal } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing(50),
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(4)
+  }
+})
+
+function getModalStyle () {
+  return {
+    width: '1074px',
+    maxWidth: '100%',
+    maxHeight: '100%',
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    overflowY: 'auto',
+    textAlign: 'center'
+  }
+}
 
 const Picture = forwardRef((props, ref) => {
-  useEffect(() => {
-    console.log('picture', props.visible)
-  })
-  return <>{props.visible ? <img ref={ref} src={props.url}/> : ''}</>
+  const { classes } = props
+
+  return <>
+  <Modal
+    open={props.visible}
+    aria-labelledby="simple-modal-title"
+    aria-describedby="simple-modal-description"
+    style={{ alignItems: 'center', justifyContent: 'center' }}
+
+  ><div style={getModalStyle()} className={classes.paper}>
+    <img width='100%' ref={ref} src={props.url}/>
+    <p>Please stay with us...</p>
+  </div></Modal>
+  <Modal
+    open={props.buttonVisible}
+    aria-labelledby="simple-modal-title"
+    aria-describedby="simple-modal-description"
+    style={{ alignItems: 'center', justifyContent: 'center' }}
+
+  ><div style={getModalStyle()} className={classes.paper}>
+    <Fab
+        color='primary'
+        variant='extended'
+        size='large'
+        onClick={props.handleRestart}
+        >Thank you. Would you like to do it again?
+    </Fab>
+  </div></Modal>
+  </>
 })
 
 Picture.displayName = 'Picture'
 Picture.propTypes = {
-  visible: PropTypes.bool,
-  url: PropTypes.string
+  visible: PropTypes.bool.isRequired,
+  buttonVisible: PropTypes.bool.isRequired,
+  url: PropTypes.string.isRequired,
+  handleRestart: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired // withStyles
 }
 
-export default Picture
+const PictureWrapped = withStyles(styles)(Picture)
+export default PictureWrapped
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component, createRef } from 'react'
+import { Container } from '@material-ui/core'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import CollectExpression from '../components/CollectExpression'
@@ -22,6 +23,7 @@ class Home extends Component {
     this.handleExpressions = this.handleExpressions.bind(this)
     this.handleStatusChange = this.handleStatusChange.bind(this)
     this.handleRestart = this.handleRestart.bind(this)
+    this.closePicture = this.closePicture.bind(this)
   }
 
   handleExpressions (expressions) {
@@ -60,41 +62,50 @@ class Home extends Component {
     this.setState({ status })
   }
 
+  closePicture () {
+    this.setState({ isPictureVisible: false })
+  }
+
   handleRestart () {
     setTimeout(() => { this.setState(initialStatus) }, 1000)
   }
 
   render () {
     return (
-      <div className={styles.container}>
+      <Container maxWidth="lg">
         <Head>
           <title>Mood Lifter</title>
           <link rel="icon" href="/favicon.ico" />
           <script defer src="/face-api.min.js"></script>
         </Head>
 
+        <Picture
+          ref={this.pictureRef}
+          visible={this.state.isPictureVisible}
+          buttonVisible={!!this.state.expressions['second-reaction']}
+          handleRestart={this.handleRestart}
+          url={this.state.pictureUrl}
+        />
         <main className={styles.main}>
           <h1 className={styles.title}>
             Mood Lifter
           </h1>
-          <Picture ref={this.pictureRef} visible={this.state.isPictureVisible} url={this.state.pictureUrl}/>
+
+          <p className={styles.description}>
+            {this.state.result}
+          </p>
           <CollectExpression
             detectionName={this.state.currentDetectionName}
             setStatus={this.handleStatusChange}
             onExpressions={this.handleExpressions}
             ref={this.expressionsRef}
           />
-          <p className={styles.description}>
-            {this.state.result}
-          </p>
-          {this.state.expressions['second-reaction'] ? <button onClick={this.handleRestart}>AGAIN!</button> : ''}
-          {this.state.expressions['second-reaction'] ? <pre className={styles.json}><code>{JSON.stringify(this.state.expressions, null, 2)}</code></pre> : ''}
         </main>
 
         <footer className={styles.footer}>
           Powered by Love | {this.state.currentDetectionName} &gt; {this.state.status}
         </footer>
-      </div>
+      </Container>
     )
   }
 }
