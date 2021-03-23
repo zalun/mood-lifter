b44088b98a573e5938732d5da715d16a38642d36
Piotr Zalewa <zaloon@gmail.com>
2021-03-23 10:26:06 +0100

    about page with zero content

--- pages/about.js
+++ pages/about.js
@@ -3,18 +3,20 @@ import { EXPRESSIONS } from '../helpers/expressions'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-
-export default function About () {
+export default function About() {
   return (
-    <Container maxWidth="lg"> 
-		  <Head>
-				<title>Mood Lifter</title>
-				<link rel="icon" href="/favicon.ico" />
-				<script defer src="/face-api.min.js"></script>
-			</Head>
-			<main className={styles.main}>
-				<h1>Mood Lifter</h1>
-			</main>
+    <Container maxWidth="lg">
+      <Head>
+        <title>Mood Lifter</title>
+        <link rel="icon" href="/favicon.ico" />
+        <script defer src="/face-api.min.js"></script>
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Mood Lifter</h1>
+        <p>
+          Here we will write some words about the project - technology, authors, etc.
+        </p>
+      </main>
     </Container>
   )
 }
